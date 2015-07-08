50ba78aa1d9a6bc8c8b641e609b0986249cd3c44
Jamie Yang <jamie@urad.com.tw>
2015-07-08 14:58:46 +0800

    add app.js + comment

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// 把index 裡的all 拿來用
+var all = require('./index');
+var koa = require('koa');
+var app = koa();
+
+// koa的機制console.log出來
+// 如果只有用 all(); 來call function 的話 console.log 不會出來 
+app.use(all);
+
+// 在localhost:8080頁面上顯示 hello world
+app.use(function *(next){
+    this.body = 'Hello World';
+    yield next;
+});
+
+app.listen(8080);
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var koa = require('koa');
-var app = module.exports = koa();
+var app = koa();
 
-module.exports = all;
+module.exports = all;  // export module讓其他程式可以使用
+
 function responseTime(prepend, append) {
     return function *(next) {
         var start = new Date;
         yield next;
         var ms = new Date - start;
-        console.log('$s - %s %s', prepend, ms, append);
+        console.log('%s %s', prepend, ms, append);
     }
 }
 
@@ -23,12 +24,7 @@ function logger(){
 }
 
 function *all(next){
-    yield responseTime("response time", "ms").call(this, logger().call(this, next));
+    // 用call method 在logger middleware 裡面 call responseTime() middleware
+    // 也可以先後順序交換 output就會不同
+    yield logger().call(this, responseTime("response time", "ms").call(this, next));
 }
-app.use(all);
-app.use(function *(next){
-    this.body = 'Hello World';
-    yield next;
-});
-app.listen(3000);
-
