1be2ce6fc3281554b6c336b8dfcb22b0123da369
Jamie Yang <jamie@urad.com.tw>
2015-07-08 15:08:15 +0800

    add comment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var koa = require('koa');
 var app = koa();
 
 // koa的機制console.log出來
-// 如果只有用 all(); 來call function 的話 console.log 不會出來 
+// 如果只有用 all(); 來call function 的話 只有all 這個function的結果會出來 
 app.use(all);
 
 // 在localhost:8080頁面上顯示 hello world
